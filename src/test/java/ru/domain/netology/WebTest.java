ebedd0edb0346909378eddfb840b9a99dc163f87
bairb <bairu@yandex.ru>
2021-10-26 21:38:32 +0300

    webtest modified

--- src/test/java/ru/domain/netology/WebTest.java
+++ src/test/java/ru/domain/netology/WebTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WebTest {
 
     private WebDriver driver;
@@ -38,23 +40,27 @@ public class WebTest {
     }
 
     @Test
-    void shouldTestSomething() {
+    void shouldSubmitRequest(){
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79263334455");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Евгений");
+        elements.get(1).sendKeys("+79031234567");
         driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
-        String message = driver.findElement(By.className("paragraph")).getText();
-        Assertions.assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", message);
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
     }
 
-
     @Test
-    void shouldTestWrongName() {
+    void shouldRemainError(){
         driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Ivanov Ivan");
-        driver.findElement(By.cssSelector("button[type='button']")).click();
-        String message = driver.findElement(By.cssSelector("span[data-test-id = \"name\"] span + span +span")).getText();
-        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", message);
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Ivan Pe");
+        elements.get(1).sendKeys("+79031234567");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 }
